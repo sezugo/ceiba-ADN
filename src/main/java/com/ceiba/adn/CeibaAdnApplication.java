a16847a2d7ed96532893d140e5c9f05627e39652
Sebastián Zuluaga Gómez - Ceiba Software <sebastian.zuluaga@ceiba.com.co>
2020-12-17 07:34:42 -0500

    Proyecto Inicial

---
+++ src/main/java/com/ceiba/adn/CeibaAdnApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.adn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeibaAdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeibaAdnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceiba/adn/CeibaAdnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.adn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeibaAdnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
